a82a9f2c3ee2dfede9cb919bdc88035a262b9e6c
sumaya <sumaya.shahoyi@gmail.com>
2018-09-06 08:20:39 +0300

    add

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,8 @@
 // Code ControlledInput Component Here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Mount Components Here</div>,
+  document.getElementById('root')
+);
